c42b8c79a590f98609c4066cb98f868157fc2474
Thomas_L <thomas.lemoigne@yahoo.fr>
2025-12-12 12:10:50 +0100

    update : rien ne marche

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 import Carte from "./components/Carte";
-import Panneau from "./components/Panneau";
 import InfoPanel from "./components/InfoPanel";
+import Panneau from "./components/Panneau";
+import "./index.css";
 
 function App() {
   const [map, setMap] = useState(null);
 
   return (
     <div>
-      <Carte setMap={setMap} />
+      {/* Wrapper plein écran pour la carte */}
+      <div
+        id="map-wrapper"
+        style={{ width: "100%", height: "100vh", position: "relative", zIndex: 0 }}
+      >
+        <Carte setMap={setMap} />
+      </div>
+
+      {/* Panneau info */}
       {map && <InfoPanel map={map} />}
+
+      {/* Panneau droit */}
       <Panneau />
     </div>
   );
--- src/components/Carte.jsx
+++ src/components/Carte.jsx
@@ -1,35 +1,23 @@
 import React, { useEffect } from "react";
 import maplibregl from "maplibre-gl";
-import { Protocol } from "pmtiles";
 
 export default function Carte({ setMap }) {
   useEffect(() => {
-    const protocol = new Protocol();
-    maplibregl.addProtocol("pmtiles", protocol.tile);
-
     const mapInstance = new maplibregl.Map({
       container: "map",
-      style: {
-        version: 8,
-        sources: {
-          "openmaptiles": {
-            type: "vector",
-            url: "pmtiles://https://osm.nbg1.your-objectstorage.com/planet.pmtiles"
-          }
-        },
-        layers: [
-          { id: "background", type: "background", paint: { "background-color": "#45516E" } }
-        ],
-        center: [-0.25979, 48.70337],
-        zoom: 7.5
-      }
+      style: "https://api.maptiler.com/maps/land-gradient-dark/style.json?key=xX6tlHbHqnG4ROor79FN",
+      center: [-0.25979, 48.70337],
+      zoom: 7.5
     });
 
     mapInstance.addControl(new maplibregl.NavigationControl());
-
-    // Renvoyer l’instance au parent pour InfoPanel
     setMap(mapInstance);
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "100vh" }}></div>;
+  return (
+    <div
+      id="map"
+      style={{ width: "100%", height: "100%", position: "relative", zIndex: 0 }}
+    ></div>
+  );
 }
